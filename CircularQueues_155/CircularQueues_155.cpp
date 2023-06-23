8a72fb8a61ead0a0c89df8ce38392b0c806b196f
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-06-23 09:39:06 +0700

    cek jika antrian hanya memiliki satu element

--- CircularQueues_155/CircularQueues_155.cpp
+++ CircularQueues_155/CircularQueues_155.cpp
@@ -47,9 +47,12 @@ public:
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
 		// cek jika antrian hanya memiliki satu element 
-		if (FRONT == max - 1)
-			FRONT = 0;
-		else
-			FRONT = FRONT + 1;
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+
+
+		
 	}
 };
