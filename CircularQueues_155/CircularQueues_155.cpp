62f2b742b8f96c2f069f67471dc43fe3c4a13c3d
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-06-23 11:07:09 +0700

    Menambahkan algorithma

--- CircularQueues_155/CircularQueues_155.cpp
+++ CircularQueues_155/CircularQueues_155.cpp
@@ -89,7 +89,12 @@ public:
 			
 			FRONT_position = 0;
 
-
+			// iterasi dari awal array hingga rear
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 	}
 };
