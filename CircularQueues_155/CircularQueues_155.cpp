ba54d1be706bb86e40442a717311750a5b64adbe
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-06-23 09:22:41 +0700

    Membuat classs Queues

--- CircularQueues_155/CircularQueues_155.cpp
+++ CircularQueues_155/CircularQueues_155.cpp
@@ -5,22 +5,4 @@ class queues {
 	int FRONT, REAR, max = 5;
 	int queue_array[5];
 
-public:
-	Queues() {
-		FRONT = -1;
-		REAR = -1;
-	}
-
-	void insert() {
-		int num;
-		cout << "Enter a number: ";
-		cin >> num;
-		cout << endl;
-
-		// cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-			cout << "\n Queue overflow\n";
-			return;
-		}
-	}
 };
